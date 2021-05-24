715340451868f6f741fd95a8eaa74c808528469d
Hoel Kervadec <hoel@kervadec.science>
2021-05-24 18:31:50 -0400

    Dataset pipeline type check

--- src/datasets/pipeline.py
+++ src/datasets/pipeline.py
@@ -1,28 +1,28 @@
-import torchvision
-import torch
-import numpy as np
 from typing import List, Union
+
+import torch
+import torchvision
+import numpy as np
 import cv2
 from PIL import Image
-import sys
 
-from .utils import Split
 from . import reader
 from . import sampling
+from .utils import Split
 from .transform import get_transforms
-from .dataset_spec import HierarchicalDatasetSpecification as HDS
-from .dataset_spec import BiLevelDatasetSpecification as BDS
-from .dataset_spec import DatasetSpecification as DS
-from .config import EpisodeDescriptionConfig, DataConfig
-from .tfrecord.torch.dataset import TFRecordDataset
 from .sampling import EpisodeDescriptionSampler
+from .tfrecord.torch.dataset import TFRecordDataset
+from .config import EpisodeDescriptionConfig, DataConfig
+from .dataset_spec import DatasetSpecification as DS
+from .dataset_spec import BiLevelDatasetSpecification as BDS
+from .dataset_spec import HierarchicalDatasetSpecification as HDS
 
 
 def make_episode_pipeline(dataset_spec_list: List[Union[HDS, BDS, DS]],
                           split: Split,
                           episode_descr_config: EpisodeDescriptionConfig,
                           data_config: DataConfig,
-                          ignore_hierarchy_probability: int = 0.0,
+                          ignore_hierarchy_probability: float = 0.0,
                           **kwargs):
     """Returns a pipeline emitting data from potentially multiples source as Episodes.
 
@@ -45,6 +45,7 @@ def make_episode_pipeline(dataset_spec_list: List[Union[HDS, BDS, DS]],
                                        split=split,
                                        shuffle=data_config.shuffle,
                                        offset=offset)
+
         class_datasets = episode_reader.construct_class_datasets()
         sampler = sampling.EpisodeDescriptionSampler(
             dataset_spec=episode_reader.dataset_spec,
@@ -53,7 +54,9 @@ def make_episode_pipeline(dataset_spec_list: List[Union[HDS, BDS, DS]],
             use_dag_hierarchy=episode_descr_config.use_dag_ontology_list[i],
             use_bilevel_hierarchy=episode_descr_config.use_bilevel_ontology_list[i],
             ignore_hierarchy_probability=ignore_hierarchy_probability)
+
         transforms = get_transforms(data_config, split)
+
         _, max_support_size, max_query_size = sampler.compute_chunk_sizes()
         episodic_dataset_list.append(EpisodicDataset(class_datasets=class_datasets,
                                                      sampler=sampler,
@@ -78,7 +81,7 @@ def make_batch_pipeline(dataset_spec_list: List[Union[HDS, BDS, DS]],
     """
 
     offset = 0
-    dataset_list = []
+    dataset_list: List[BatchDataset] = []
     for dataset_spec in dataset_spec_list:
         batch_reader = reader.Reader(dataset_spec=dataset_spec,
                                      split=split,
@@ -92,8 +95,8 @@ def make_batch_pipeline(dataset_spec_list: List[Union[HDS, BDS, DS]],
                                transforms=transforms)
         dataset_list.append(dataset)
         offset += len(class_datasets)
-    dataset = ZipDataset(dataset_list)
-    return dataset
+
+    return ZipDataset(dataset_list)
 
 
 class EpisodicDataset(torch.utils.data.IterableDataset):
@@ -103,7 +106,8 @@ class EpisodicDataset(torch.utils.data.IterableDataset):
                  transforms: torchvision.transforms,
                  max_support_size: int,
                  max_query_size: int):
-        super(EpisodicDataset).__init__()
+        super().__init__()
+
         self.class_datasets = class_datasets
         self.sampler = sampler
         self.transforms = transforms
@@ -113,43 +117,56 @@ class EpisodicDataset(torch.utils.data.IterableDataset):
 
     def __iter__(self):
         while True:
-            episode_description = self.sampler.sample_episode_description(self.random_gen)
             support_images = []
             support_labels = []
             query_images = []
             query_labels = []
+
+            episode_description = self.sampler.sample_episode_description(self.random_gen)
             episode_classes = list({class_ for class_, _, _ in episode_description})
+
             for class_id, nb_support, nb_query in episode_description:
                 used_ids = []
                 sup_added = 0
                 query_added = 0
+
                 while sup_added < nb_support:
                     sample_dic = self.get_next(class_id)
+
                     if sample_dic['id'] not in used_ids:
                         sample_dic = parse_record(sample_dic)
                         used_ids.append(sample_dic['id'])
+
                         support_images.append(self.transforms(sample_dic['image']).unsqueeze(0))
                         sup_added += 1
+
                 while query_added < nb_query:
                     sample_dic = self.get_next(class_id)
+
                     if sample_dic['id'] not in used_ids:
                         sample_dic = parse_record(sample_dic)
+
                         used_ids.append(sample_dic['id'])
                         query_images.append(self.transforms(sample_dic['image']).unsqueeze(0))
+
                         query_added += 1
+
                 # print(f"Class {class_id} contains duplicate: {contains_duplicates(used_ids)}")
                 support_labels.extend([episode_classes.index(class_id)] * nb_support)
                 query_labels.extend([episode_classes.index(class_id)] * nb_query)
+
             support_images = torch.cat(support_images, 0)
             query_images = torch.cat(query_images, 0)
+
             support_labels = torch.tensor(support_labels)
             query_labels = torch.tensor(query_labels)
+
             yield support_images, query_images, support_labels, query_labels
 
     def get_next(self, class_id):
         try:
             sample_dic = next(self.class_datasets[class_id])
-        except:
+        except StopIteration:
             self.class_datasets[class_id] = cycle_(self.class_datasets[class_id])
             sample_dic = next(self.class_datasets[class_id])
         return sample_dic
@@ -159,7 +176,8 @@ class BatchDataset(torch.utils.data.IterableDataset):
     def __init__(self,
                  class_datasets: List[TFRecordDataset],
                  transforms: torchvision.transforms):
-        super(BatchDataset).__init__()
+        super().__init__()
+
         self.class_datasets = class_datasets
         self.transforms = transforms
         self.random_gen = np.random.RandomState()
@@ -176,15 +194,16 @@ class BatchDataset(torch.utils.data.IterableDataset):
     def get_next(self, class_id):
         try:
             sample_dic = next(self.class_datasets[class_id])
-        except:
+        except StopIteration:
             self.class_datasets[class_id] = cycle_(self.class_datasets[class_id])
             sample_dic = next(self.class_datasets[class_id])
+
         return sample_dic
 
 
 class ZipDataset(torch.utils.data.IterableDataset):
     def __init__(self,
-                 dataset_list: List[EpisodicDataset]):
+                 dataset_list: Union[List[EpisodicDataset], List[BatchDataset]]):
         self.dataset_list = dataset_list
         self.random_gen = np.random.RandomState()
 
@@ -192,14 +211,16 @@ class ZipDataset(torch.utils.data.IterableDataset):
         while True:
             rand_source = self.random_gen.randint(len(self.dataset_list))
             next_e = self.get_next(rand_source)
+
             yield next_e
 
     def get_next(self, source_id):
         try:
             dataset = next(self.dataset_list[source_id])
-        except:
+        except StopIteration:
             self.dataset_list[source_id] = iter(self.dataset_list[source_id])
             dataset = next(self.dataset_list[source_id])
+
         return dataset
 
 
@@ -207,9 +228,11 @@ def worker_init_fn_(worker_id, seed):
     worker_info = torch.utils.data.get_worker_info()
     dataset = worker_info.dataset  # the dataset copy in this worker process
     random_gen = np.random.RandomState(seed + worker_id)
+
     dataset.random_gen = random_gen
     for source_dataset in dataset.dataset_list:
         source_dataset.random_gen = random_gen
+
         for class_dataset in source_dataset.class_datasets:
             class_dataset.random_gen = random_gen
 
@@ -238,6 +261,7 @@ def parse_record(feat_dic):
     image = Image.fromarray(image)
     feat_dic["image"] = image
     return feat_dic
+
 
 def contains_duplicates(X):
     return len(np.unique(X)) != len(X)
